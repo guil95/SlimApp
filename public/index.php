709cd31d526a5666c619f2fb2991aac5c72b5b5f
Guilherme Rodrigues <guilhermerodriguestb@gmail.com>
2019-06-11 13:36:52 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,15 +5,15 @@ $app = new \App\Application([
     'settings' => [
         'displayErrorDetails' => true,
         'determineRouteBeforeAppMiddleware' => true,
-    ],
-    'db' => [
-        'driver'    => 'pdo_mysql',
-        'charset'   => 'utf8',
-        'host'      => '127.0.0.1',
-        'port'      => 3306,
-        'dbname'    => 'db_name',
-        'user'      => 'user',
-        'password'  => 'password'
+        'db' => [
+            'driver'    => 'pdo_mysql',
+            'charset'   => 'utf8',
+            'host'      => '127.0.0.1',
+            'port'      =>  3306,
+            'dbname'    => 'db_name',
+            'user'      => 'user',
+            'password'  => 'password'
+        ]
     ]
 ]);
 
