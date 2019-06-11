516feefcbfd2bd71fec31456fcdc61ab7a0434ed
Guilherme Rodrigues <guilhermerodriguestb@gmail.com>
2019-06-11 13:39:37 -0300

    Update containers.php

--- config/containers.php
+++ config/containers.php
@@ -5,3 +5,12 @@ $container[\App\Services\MyService::class] = function ($container) {
     $myService = new \App\Services\MyService();
     return $myService;
 };
+
+$container['db'] = function ($container) {
+    $db = $container['settings']['db'];
+    $pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
+    $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
